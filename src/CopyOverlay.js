512db0e220d0ae2ac7b24212d11e542803f2fedd
Elie Schoppik <eschoppik@gmail.com>
2019-02-12 17:04:28 -0800

    pure component and cleaner css

--- src/CopyOverlay.js
+++ src/CopyOverlay.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import "./CopyOverlay.css";
 
-class CopyOverlay extends Component {
+class CopyOverlay extends PureComponent {
   render() {
     return (
       <div
